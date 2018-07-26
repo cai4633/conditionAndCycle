6753f819bb22bd8725294c803d731fabfb026f6e
cai4633 <463353522@qq.com>
2018-07-26 23:43:21 +0800

    v1.0.2 增加了组件通信练习

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
 // import Vue from 'vue';
+var sonComponent={
+	data:function(){
+		return {
+		}
+	},
+	props:['value'],
+	template:`<div id="container"><input type="text" id="son" :value="value"  @input="$emit('input',$event.target.value)" ><button  @click=" $emit('show') && $parent.$emit('show')">通知父组件</button></div>`/*通过$parent来跨层发射事件*/
+},
+	fatherComponent={
+		data:function(){
+			return{text:'',
+					activeColor:'',
+					showText:'静静的等待用户输入'
+					}
+				},
+		methods:{
+			showSomething:function(){
+				this.showText=this.text.trim()?this.text:'静静的等待用户输入';
+				this.$parent.text=this.text;//组件之间传递数据；
+				this.activeColor=this.text.trim()?'#61EA74':'';/*改变样式*/
+				
+			},
+			changeSon:function(){
+
+			}
+		},
+		template:`<div><h1>组件通信练习 </h1><sonComponent v-model='text' @show='showSomething' ></sonComponent><div :style='{color:activeColor}'><strong>我是父组件：</strong><span contenteditable='true' @input='changeSon'>{{showText}}</span></div></div>`,
+		components:{
+			'sonComponent':sonComponent
+		}
+	};
+
+Vue.component("son-component",sonComponent);
+Vue.component("father-component",fatherComponent);
+
 var app1=new Vue({
 	el:'#app1',
 	data:{
@@ -28,7 +63,9 @@ var app1=new Vue({
 		}
 	}
 
-}),app2=new Vue({
+}),
+
+app2=new Vue({
 	el:'#app2',
 	data:{
 		styleObject:{background:'red',width:'100px',height:'100px'}
@@ -37,5 +74,20 @@ var app1=new Vue({
 }
 
 }
+),
+app3=new Vue({
+	el:'#app3',
+	data:{
+		showText:'我也静静等待用户输入！',
+		activeColor:''
+	},
+		methods:{
+		showSomething:function(){
+			this.showText=this.text.trim()?this.text:'我也静静等待用户输入！';
+			this.activeColor=this.text.trim()?'#F43BE7':'';/*改变样式*/
+			
+		}
+	}
 
-	);
+});
+
